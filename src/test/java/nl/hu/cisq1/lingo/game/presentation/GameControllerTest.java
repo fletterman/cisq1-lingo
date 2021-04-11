305ce1515f42a4b69ebb90cc4e8f7ad683a47839
fletterman <stijn.castrop@me.com>
2021-04-11 19:45:49 +0200

    Update build.yml

--- src/test/java/nl/hu/cisq1/lingo/game/presentation/GameControllerTest.java
+++ src/test/java/nl/hu/cisq1/lingo/game/presentation/GameControllerTest.java
@@ -1,8 +1,12 @@
 package nl.hu.cisq1.lingo.game.presentation;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import nl.hu.cisq1.lingo.CiTestConfiguration;
 import nl.hu.cisq1.lingo.game.data.SpringGameRepository;
 import nl.hu.cisq1.lingo.game.domain.Game;
+import nl.hu.cisq1.lingo.game.domain.exception.InvalidWordLength;
+import nl.hu.cisq1.lingo.game.domain.exception.RoundAlreadyPlayingException;
+import nl.hu.cisq1.lingo.game.presentation.dto.AttemptDTO;
 import nl.hu.cisq1.lingo.words.data.SpringWordRepository;
 import nl.hu.cisq1.lingo.words.domain.Word;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,13 +17,16 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.Optional;
 
+import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Import(CiTestConfiguration.class)
@@ -48,34 +55,34 @@ public class GameControllerTest {
         mockMvc.perform(requestBuilder).andExpect(status().isOk());
     }
 
-//    @Test
-//    @DisplayName("Making new round")
-//    void newRound() throws Exception {
-//        when(wordRepository.findRandomWordByLength(5)).thenReturn(Optional.of(new Word("tests")));
-//        when(gameRepository.findById(0L)).thenReturn(Optional.of(game));
-//        RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/game/game/0/round");
-//        mockMvc.perform(requestBuilder)
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.state", is("PLAYING")))
-//                .andExpect(jsonPath("$.score", is(0)))
-//                .andExpect(jsonPath("$.hint", is("t....")))
-//                .andExpect(jsonPath("$.round", is(1)));
-//    }
-//
-//    @Test
-//    @DisplayName("Making a guess")
-//    void guess() throws InvalidWordLength, RoundAlreadyPlayingException, Exception {
-//        game.newRound("tests");
-//        when(gameRepository.findById(0L)).thenReturn(Optional.of(game));
-//        AttemptDTO attemptDTO = new AttemptDTO("plats");
-//        String guessBody = new ObjectMapper().writeValueAsString(attemptDTO);
-//        RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/game/game/0/guess").contentType(MediaType.APPLICATION_JSON).content(guessBody);
-//        mockMvc.perform(requestBuilder)
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.state", is("PLAYING")))
-//                .andExpect(jsonPath("$.score", is(25)))
-//                .andExpect(jsonPath("$.hint", is("t..ts")));
-//    }
+    @Test
+    @DisplayName("Making new round")
+    void newRound() throws Exception {
+        when(wordRepository.findRandomWordByLength(5)).thenReturn(Optional.of(new Word("tests")));
+        when(gameRepository.findById(0L)).thenReturn(Optional.of(game));
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/game/game/0/round");
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.state", is("PLAYING")))
+                .andExpect(jsonPath("$.score", is(0)))
+                .andExpect(jsonPath("$.hint", is("t....")))
+                .andExpect(jsonPath("$.round", is(1)));
+    }
+
+    @Test
+    @DisplayName("Making a guess")
+    void guess() throws InvalidWordLength, RoundAlreadyPlayingException, Exception {
+        game.newRound("tests");
+        when(gameRepository.findById(0L)).thenReturn(Optional.of(game));
+        AttemptDTO attemptDTO = new AttemptDTO("plats");
+        String guessBody = new ObjectMapper().writeValueAsString(attemptDTO);
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/game/game/0/guess").contentType(MediaType.APPLICATION_JSON).content(guessBody);
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.state", is("PLAYING")))
+                .andExpect(jsonPath("$.score", is(25)))
+                .andExpect(jsonPath("$.hint", is("t..ts")));
+    }
 //
 //    @Test
 //    @DisplayName("Making a guess when round hasn't started yet")
@@ -86,7 +93,7 @@ public class GameControllerTest {
 //        RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/game/game/0/guess").contentType(MediaType.APPLICATION_JSON).content(guessBody);
 //        mockMvc.perform(requestBuilder).andExpect(jsonPath("$.errorCode").value("CONFLICT"));
 //    }
-//
+
 //    @Test
 //    @DisplayName("Starting round with no game active")
 //    void noGame() throws Exception {
